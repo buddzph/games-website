78c14a1c637a00a34140bef3907b681817a5b59c
Frederick de Guzman <frederick@glyphgames.com>
2017-08-24 14:06:46 +0800

    updates

--- wp-content/themes/twentyseventeen/template-parts/login-process.php
+++ wp-content/themes/twentyseventeen/template-parts/login-process.php
@@ -547,18 +547,8 @@ switch ($_REQUEST['func']) {
 
 			$getcode = smartcurl($mobile_number);
 
-			$decoderes = json_decode($getcode);
 
-			foreach ($getcode as $key => $value) {
-				echo 'Key: '.$key.' value: '.$value;
-			}
-
-			foreach ($decoderes as $key => $value) {
-				echo 'Key: '.$key.' value: '.$value;
-			}
-
-
-			// echo "<pre>"; print_r($decoderes); echo "</pre>";
+			echo "<pre>"; print_r($getcode); echo "</pre>";
 
 			echo $ClientReferenceNumber = $getcode['response']['ClientReferenceNumber'];
 
